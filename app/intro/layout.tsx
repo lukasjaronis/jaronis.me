91e0829bc28217d51244b2ab65fa1881f2c822c1
Lukas Jaronis <jaronisluke13@gmail.com>
2023-12-04 16:50:36 -0800

    add open graph to intro

--- app/intro/layout.tsx
+++ app/intro/layout.tsx
@@ -3,6 +3,13 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Intro',
   description: 'Introduction',
+  openGraph: {
+    type: "website",
+    url: "https://yebuntu.vercel.app/intro",
+    title: "Intro",
+    description: "Quick introduction",
+    siteName: "Yebuntu",
+  }
 }
 
 export default function IntroLayout({
