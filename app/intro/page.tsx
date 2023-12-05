d485ec514df9a11f678fd0ce2565bb99a1be9db4
Lukas Jaronis <jaronisluke13@gmail.com>
2023-12-05 00:15:51 -0800

    yes

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -3,28 +3,17 @@ export default function Intro() {
     <section className="flex items-center justify-center h-[calc(100vh_-_128px)]">
       <div className="max-w-md">
         <ul className="text-center space-y-2">
+          <li>Lukas Jaronis</li>
+          <li>Full-stacker with 5+ years experience</li>
           <li>
-            Lukas Jaronis
-          </li>
-          <li>
-            Full-stacker with 5+ years experience
-          </li>
-          <li>
-             Expert in JavaScript/TypeScript, NodeJS, React, and familiar with a wide range of other technologies
+            Expert in JavaScript/TypeScript, NodeJS, React, and familiar with a
+            wide range of other technologies
           </li>
           <li>
             Excitment for designing, developing, and managing web applications
           </li>
-          <li>
-            Bilingual proficiency in Lithuanian & English
-          </li>
-          <li>
-            I like <span className="text-yellow-300">Cloudflare Workers</span> and I don't mind 
-            jumping into really <span className="text-cyan-300">cold</span> lakes
-          </li>
-          <li>
-            im quick (not typing, sprinting)
-          </li>
+          <li>Bilingual proficiency in Lithuanian & English</li>
+          <li>Fan of Cloudflare, Vercel, Axiom, DrizzleORM, and abunchaofotherstuffprobably</li>
         </ul>
       </div>
     </section>
