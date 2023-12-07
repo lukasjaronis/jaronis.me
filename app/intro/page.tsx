de6e87fb371998703a78976ba738818b2af69d5c
Lukas Jaronis <jaronisluke13@gmail.com>
2023-12-07 13:17:43 -0800

    add link

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Intro() {
 	return (
 		<section className="flex items-center justify-center h-[calc(100vh_-_128px)]">
@@ -6,7 +8,7 @@ export default function Intro() {
 					<p className="text-4xl text-firefly-100 text-center md:text-left font-geist_mono">Lukas Jaronis</p>
 					<p className="text-sm text-firefly-400 text-center md:text-left font-geist_mono">full-stacker with 5+ yoe</p>
 				</div>
-				<p className="md:indent-10">Finishing up Gate which is the 'genesis' service for a 1-click serverless service incumbator which
+				<p className="md:indent-10">Finishing up <Link href="/projects" className="underline underline-offset-4 font-black hover:text-firefly-100">Gate</Link> which is the 'genesis' service for a 1-click serverless service incumbator which
 					aims to provide products ownership of services they need, but much more affordable and flexible towards their own scopes.
 				</p>
 			</div>
