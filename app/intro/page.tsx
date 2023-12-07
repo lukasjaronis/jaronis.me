fa5899175111b1a1e1aa827b2b42c1cf0d06f799
Lukas Jaronis <jaronisluke13@gmail.com>
2023-12-07 11:55:06 -0800

    update intro, i like this better

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -1,24 +1,14 @@
 export default function Intro() {
 	return (
 		<section className="flex items-center justify-center h-[calc(100vh_-_128px)]">
-			<div className="max-w-md">
-				<p className="text-2xl text-ice-cold-500 text-center">Lukas Jaronis</p>
-				<p className="text-center">full-stacker with 5+ yoe</p>
-				<div className="mt-5 flex flex-col items-center justify-center text-center gap-5">
-					<p className="text-firefly-400">
-						Expert in JavaScript/TypeScript, NodeJS, React, and familiar with a wide
-						range of other technologies
-					</p>
-					<p className="text-firefly-400">
-						Excitment for designing, developing, and managing web applications
-					</p>
-					<p className="text-firefly-400">
-						Bilingual proficiency in Lithuanian & English
-					</p>
-					<p className="text-firefly-400">
-						Favorite tech for me right now is Cloudflare, Vercel, Axiom, and DrizzleORM
-					</p>
+			<div className="max-w-xl flex flex-col w-full">
+				<div className="self-center mb-10">
+					<p className="text-4xl text-firefly-100 text-center md:text-left font-geist_mono">Lukas Jaronis</p>
+					<p className="text-sm text-firefly-400 text-center md:text-left font-geist_mono">full-stacker with 5+ yoe</p>
 				</div>
+				<p className="md:indent-10">Finishing up Gate which is the 'genesis' service for a 1-click serverless service incumbator which
+					aims to provide products ownership of services they need, but much more affordable and flexible towards their own scopes.
+				</p>
 			</div>
 		</section>
 	);
