67b60b304060a0c84e8a8102177fbc6d54c365fa
Lukas Jaronis <jaronisluke13@gmail.com>
2023-12-13 16:34:40 -0800

    update intro

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -8,9 +8,10 @@ export default function Intro() {
 					<p className="text-4xl text-firefly-100 text-center md:text-left font-geist_mono">Lukas Jaronis</p>
 					<p className="text-sm text-firefly-400 text-center md:text-left font-geist_mono">full-stacker with 5+ yoe</p>
 				</div>
-				<p className="text-center md:indent-10">Finishing up <Link href="/projects" className="underline underline-offset-4 font-black hover:text-firefly-100">Gate</Link> which is the 'genesis' service for a 1-click serverless service incubator which
-					aims to provide products ownership of services they need, but much more affordable and flexible provided their own scopes.
+				<p className="text-center md:indent-10">Working on serverless services both for pleasure and educative means,
+					where I aim to provide self-hosted <i>templates</i> with the bells and whistles needed for any product.
 				</p>
+				<p className="pt-2 text-center md:indent-10">Currently focusing on <Link href="/projects" className="underline underline-offset-4 font-black hover:text-firefly-100">Gate</Link> which enables owners to create keys and verify them based on constraints they need.</p>
 			</div>
 		</section>
 	);
