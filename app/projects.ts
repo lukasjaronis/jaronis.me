b02b607fe87905579ac3e4efaa3e82f09ec65443
Lukas Jaronis <jaronisluke13@gmail.com>
2024-01-19 02:18:52 -0500

    update

--- app/projects.ts
+++ app/projects.ts
@@ -14,7 +14,8 @@ export const projects: Project[] = [
   },
   {
     name: 'Cforty',
-    description: "An intended self-hosting link shortner with superpowers (tbd on the powers). Being build on Cloudflare Workers. "
+    description: "An intended self-hosting link shortner with superpowers (tbd on the powers). Being build on Cloudflare Workers. ",
+    link: "https://brrr.ing/🥶",
   },
   {
     name: "Gate",
