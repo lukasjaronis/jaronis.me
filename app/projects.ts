44df13a4df357966154e05a969cf5783fe3ff1c2
Lukas Jaronis <jaronisluke13@gmail.com>
2024-02-04 19:07:10 -0800

    update

--- app/projects.ts
+++ app/projects.ts
@@ -2,30 +2,31 @@ export type Project = {
   name: string;
   description: string;
   demo?: string;
-  link?: string
+  link?: string;
 };
 
 export const projects: Project[] = [
   {
     name: "Abunchaparts",
     description:
-      "A native app to streamline the eBay listing process for my dads autopart scrapping business. Being built with the Tauri framework (Vite + React), Cloudflare Worker API (Hono framework), and Planetscale.",
-    demo: "https://youtu.be/DTw5lMCN8DY"
+      "A marketplace publishing & inventory web app designed for my fathers auto-part scrapping business. The first integration is eBay but is made to be flexible to add other marketplaces. Being built with NextJS, Cloudflare Workers (API), Planetscale, and DrizzleORM. Will have a proper demo out in less than a week.",
   },
   {
-    name: 'Cforty',
-    description: "An intended self-hosting link shortner with superpowers (tbd on the powers). Being build on Cloudflare Workers. ",
+    name: "Cforty",
+    description:
+      "An intended self-hosting link shortner with superpowers (tbd on the powers). Being build on Cloudflare Workers. ",
     link: "https://brrr.ing/🥶",
   },
   {
     name: "Gate",
     description:
       "An intended self-hosting API for creating and verifying API keys with optional constraints such as limited usage and or rate limiting. Built on Cloudflare workers.",
-    link: "https://github.com/lukasjaronis/jaronis.me"
+    link: "https://github.com/lukasjaronis/jaronis.me",
   },
   {
     name: "Spaced Start Club (nft project)",
-    description: "Implemented and deployed an Ethereum ERC-721 contract + UI + cloud deployment.",
+    description:
+      "Implemented and deployed an Ethereum ERC-721 contract + UI + cloud deployment.",
     link: "/posts/ssc",
   },
 ];
